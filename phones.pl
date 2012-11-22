d53b5a4bcce832bd27734fe54c077ab2333f8a23
lucas <lucasrosada@gmail.com>
2012-11-21 23:05:07 -0200

    Now the program reads from dictionary and phonebook files

--- phones.pl
+++ phones.pl
@@ -11,12 +11,13 @@
 # as the input and return an array with all possible words from
 # a dictionary.
 #
-# Start: 21:30
-#
 ################################################################################
 
 use strict;
 use warnings;
+
+use constant DICTIONARY_FILENAME => 'dict.txt';
+use constant PHONEBOOK_FILENAME => 'pbook.txt';
 
 # Hash mapping each alphabet letter to its T9 correspondence
 my $t9_map = {
@@ -87,10 +88,6 @@ sub word2t9 {
 sub make_t9_dict {
   my ($words, $t9_map) = @_;
   
-  # TODO: convert to upper case using some fancy perl expression, like map
-  ## $words[$i] = upper ($words[i]) foreach (@words);
-  #
-
   # For each word, find the T9 equivalent and put into the hash, with the number as a kye
   # and the value the array of words corresponding to that key
   my $t9_hash = {};
@@ -109,7 +106,8 @@ sub make_t9_dict {
 
 # read_peopl
 #
-# Read a phonebook composed of people names and their phone
+# Read a phonebook composed of people names and their phone. The file should be in the format
+# PERSON_NAME,PHONE
 # 
 # Parameters:
 #   $filename: phonebook filename
@@ -117,14 +115,18 @@ sub make_t9_dict {
 # Returns: a hash containing the names in the keys and the phone in the values
 #
 sub read_phonebook {
-  my ($filename) = shift;
+  my ($filename) = @_;
 
-  # TODO: stub
-  {
-    "john" => 2255,
-    "mary" => 3389,
-    "john due" => 28
+  my $people = {};
+  open FILE, "<$filename" or die "Impossible to open $filename: $!\n";
+  while (my $line = <FILE>) {
+    chomp ($line);
+    my ($name, $phone) = split (/,/, $line);
+    $people->{$name} = $phone;
   }
+  
+  close FILE;
+  return $people;
 }
 
 # read_dict
@@ -134,8 +136,20 @@ sub read_phonebook {
 # Returns: reference to array containing the words
 #
 sub read_dict {
-  my $file = shift;
+  my ($filename) = @_;
 
+  my $words = {};
+  open FILE, "<$filename" or die "Impossible to open $filename: $!\n";
+  while (my $line = <FILE>) {
+    chomp ($line);
+
+    # split the words and save them in a hash, so we don't have repetitions
+    $line =~ s/[^a-zA-Z ]//g;
+    map { $words->{uc $_} = 1 } split (/\s+/, $line)
+  }
+
+  close FILE;
+  return [keys(%$words)];
 }
 
 # phone_words
@@ -166,11 +180,12 @@ sub phone_word {
 # The main routines
 ################################################################################
 
-# The huge dictionary containing the words we know
-my $words = [qw(BALL CAKL DOG CAT YELLOW BLUE SKY AND SO ON A LOT OF WORDS IN THIS GOD DAMN WORLD ABU)];
-my $dict = make_t9_dict ($words, $t9_map);
-my $people = read_phonebook;
-
 use Data::Dumper;
+
+# The huge dictionary containing the words we know
+my $words = read_dict(DICTIONARY_FILENAME);
+my $dict = make_t9_dict ($words, $t9_map);
+my $people = read_phonebook (PHONEBOOK_FILENAME);
+
 my $a = phone_word ($people, $dict);
 print Data::Dumper->Dump ([$a]);
